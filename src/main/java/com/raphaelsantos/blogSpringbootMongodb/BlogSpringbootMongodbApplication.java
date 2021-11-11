37952dabfc53b49018ef65ec3c481c243aab29c3
Raphael Santos <raphaelsantos0712@gmail.com>
2021-11-11 14:43:15 -0300

    Project created

---
+++ src/main/java/com/raphaelsantos/blogSpringbootMongodb/BlogSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.raphaelsantos.blogSpringbootMongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphaelsantos/blogSpringbootMongodb/BlogSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelsantos.blogSpringbootMongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
