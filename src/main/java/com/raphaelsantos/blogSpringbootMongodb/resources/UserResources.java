ea9236a4e1f951e80683c6808c87077c22a05666
Raphael Santos <raphaelsantos0712@gmail.com>
2021-11-18 15:29:11 -0300

    Created user post resource

--- src/main/java/com/raphaelsantos/blogSpringbootMongodb/resources/UserResources.java
+++ src/main/java/com/raphaelsantos/blogSpringbootMongodb/resources/UserResources.java
@@ -1,18 +1,16 @@
 package com.raphaelsantos.blogSpringbootMongodb.resources;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.raphaelsantos.blogSpringbootMongodb.domain.User;
 import com.raphaelsantos.blogSpringbootMongodb.dto.UserDTO;
 import com.raphaelsantos.blogSpringbootMongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -28,9 +26,17 @@ public class UserResources {
 		return ResponseEntity.ok(listDto);
 	}
 
-	@RequestMapping(value = "/{id}")
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/raphaelsantos/blogSpringbootMongodb/services/UserService.java
+++ src/main/java/com/raphaelsantos/blogSpringbootMongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.raphaelsantos.blogSpringbootMongodb.services;
 
 import com.raphaelsantos.blogSpringbootMongodb.domain.User;
+import com.raphaelsantos.blogSpringbootMongodb.dto.UserDTO;
 import com.raphaelsantos.blogSpringbootMongodb.repositories.UserRepository;
 import com.raphaelsantos.blogSpringbootMongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO ObjDto){
+        return new User (ObjDto.getId(), ObjDto.getName(), ObjDto.getEmail());
     }
 }
