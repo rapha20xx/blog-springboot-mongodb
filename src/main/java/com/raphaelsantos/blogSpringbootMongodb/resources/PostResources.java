89ad17ad0e613686bc09cb3325bc16deafc4bb29
Raphael Santos <raphaelsantos0712@gmail.com>
2021-11-23 20:50:27 -0300

    Creating search querys

--- src/main/java/com/raphaelsantos/blogSpringbootMongodb/resources/PostResources.java
+++ src/main/java/com/raphaelsantos/blogSpringbootMongodb/resources/PostResources.java
@@ -1,17 +1,19 @@
 package com.raphaelsantos.blogSpringbootMongodb.resources;
 
 import com.raphaelsantos.blogSpringbootMongodb.domain.Post;
+import com.raphaelsantos.blogSpringbootMongodb.resources.util.URL;
 import com.raphaelsantos.blogSpringbootMongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResources {
+
 
 	@Autowired
 	private PostService postService;
@@ -20,5 +22,25 @@ public class PostResources {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = postService.findById(id);
 		return (ResponseEntity<Post>) ResponseEntity.ok().body(obj);
+	}
+
+
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value="/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/raphaelsantos/blogSpringbootMongodb/services/PostService.java
+++ src/main/java/com/raphaelsantos/blogSpringbootMongodb/services/PostService.java
@@ -6,6 +6,8 @@ import com.raphaelsantos.blogSpringbootMongodb.services.exceptions.ObjectNotFoun
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +19,14 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = postRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+    public List<Post> findByTitle(String text) {
+        return postRepository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
 
